cc8114a6e810d45fd0f1cb25ec4d5643702dc188
Jashby1993 <144633312+Jashby1993@users.noreply.github.com>
2024-04-26 23:51:47 -0600

    prep 2 complete

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,55 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("What was your score? (Please round to nearest percentile. For example, 92.8% would be input 93). ");
+        string score = Console.ReadLine();
+        char secondDigitChar = score[1];
+        int percentage = int.Parse(score);
+        string letter = "";
+        string suffix = "";
+        string adjective = "a";
+        int determiner = (int)Char.GetNumericValue(secondDigitChar);
+        if (percentage >= 90)
+        {
+            letter = "A";
+        }
+        else if (percentage >= 80 && percentage < 90)
+        {
+            letter = "B";
+        }
+        else if (percentage >= 70 && percentage < 80)
+        {
+            letter = "C";
+        }
+        else if (percentage >= 60 && percentage < 70)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+        if (determiner <= 3 && letter != "F")
+        {
+            suffix = "-";
+        }
+        else if (determiner >= 7 && letter != "A" && letter != "F")
+        {
+            suffix = "+" ;
+        }
+        if (letter == "A" || letter == "F")
+        {
+            adjective = "an";
+        }
+        Console.WriteLine($"You got {adjective} {letter}{suffix}.");
+        if (percentage >= 70)
+        {
+            Console.WriteLine("You've passed the class!");
+        }
+        else
+        {
+            Console.WriteLine("You have failed this class. Consider speaking to your mentor to see what assistance is available.");
+        }
+        
     }
 }
