7c88e2117bad8eacfc0c14257aee6844dc6bbce7
Jashby1993 <144633312+Jashby1993@users.noreply.github.com>
2024-04-27 21:15:14 -0600

    prep 4 stretch challenge completed

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,50 @@
 using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+        int biggest = 0;
+        int smallest = 99999;
+        int smallest_positive = 99999;
+        Console.Write("Please enter a number. When finished with your list, type 0. ");
+        string user_entry = Console.ReadLine();
+        int entry = int.Parse(user_entry);
+        while (entry != 0)
+        {
+            numbers.Add(entry);
+            Console.Write("Please enter another number. When finished with your list, type 0. "); 
+            user_entry = Console.ReadLine();
+            entry = int.Parse(user_entry);
+            if (entry > biggest)
+            {
+                biggest = entry;
+            }
+            if ( entry < smallest)
+            {
+                smallest = entry;
+            }
+            if (entry < smallest_positive && entry > 0)
+            {
+                smallest_positive = entry;
+            }   
+        }
+        int sum = numbers.Sum();
+        double average = (double) sum / numbers.Count;
+        string formatted_average = average.ToString("0.000");
+        numbers.Sort();
+        Console.WriteLine("The sorted list:");
+        foreach (int number in numbers)
+        { 
+            Console.WriteLine(number);
+        }
+        Console.WriteLine($"Max: {biggest}.");
+        Console.WriteLine($"Min: {smallest}");
+        Console.WriteLine($"Smallest positive: {smallest_positive}.");
+        Console.WriteLine($"Sum: {sum}");
+        Console.WriteLine($"Average: {formatted_average}");
     }
 }
