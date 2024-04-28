7800813ed60e8e94bb3f0fd3eac3e27150e059de
Jashby1993 <144633312+Jashby1993@users.noreply.github.com>
2024-04-27 21:51:36 -0600

    prep 5 complete

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -1,9 +1,32 @@
 using System;
+using System.Globalization;
 
 class Program
 {
+    static void DisplayMessage()
+    {
+        Console.WriteLine("Welcome to the program!");
+    }
+    static string get_name()
+    {   
+        Console.Write("Please enter your name: ");
+        string name = Console.ReadLine();
+        return name;
+    }
+    static int get_number()
+    {
+        Console.Write("Please enter your favorite number: ");
+        string entry = Console.ReadLine();
+        int number = int.Parse(entry);
+        return number;
+    }
+
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        DisplayMessage();
+        string name = get_name();
+        int number = get_number();
+        int squared = number * number;
+        Console.WriteLine($"{name}, your favorite number ({number}) squared is {squared}.");
     }
 }
