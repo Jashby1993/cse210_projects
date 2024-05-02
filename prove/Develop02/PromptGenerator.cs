a35caadf99a81a32f9f8f69dcd6cca351dfaebb4
Jashby1993 <144633312+Jashby1993@users.noreply.github.com>
2024-05-01 22:53:36 -0600

    All over the place.

---
+++ prove/Develop02/PromptGenerator.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+
+class PromptGenerator
+{
+    // defining attributes
+     private List<string> _prompts;
+
+    // Constructor
+    public PromptGenerator()
+    {
+        // Initialize the _prompts list in the constructor
+        _prompts = new List<string>();
+    }
+    
+}
