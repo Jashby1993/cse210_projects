87ede44bc2cd6d7fde5ad9040af705df02eb97e6
Jashby1993 <144633312+Jashby1993@users.noreply.github.com>
2024-05-01 20:38:12 -0600

    starting job class

---
+++ prepare/Learning02/Jobs.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+
+public class Job
+{
+    //defining attributes
+    public string _company;
+    public string _jobTitle;
+    public string _startYear;
+    public string _endYear;
+    
+    //defining methods
+    public void DisplayJob()
+    {
+     Console.WriteLine($"{_jobTitle} ({_company}): {_startYear} - {_endYear}");   
+    }
+}
