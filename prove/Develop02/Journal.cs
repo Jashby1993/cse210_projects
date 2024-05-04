3c3c158759b694776cb10700011ce56859ca4867
Jashby1993 <144633312+Jashby1993@users.noreply.github.com>
2024-05-04 01:45:22 -0600

    Working on journal and menu

--- prove/Develop02/Journal.cs
+++ prove/Develop02/Journal.cs
@@ -1,1 +1,14 @@
 using System;
+using System.Collections.Generic;
+using System.IO;
+
+class Journal
+{
+    public List<Entry> _entries = new List<Entry>();
+
+    public void AddEntry()
+    {
+        Entry newEntry = new Entry();
+        _entries.Add(newEntry);
+    }
+}
--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -9,5 +9,22 @@ class Program
 
         // Call the DisplayEntry method to display the entry details
         entry.DisplayEntry();
+
+        string userSelection;
+        int menuChoice;
+        Console.WriteLine("What would you like to do? Please enter the number of your choice: ")
+        Console.WriteLine("1. Get a prompt and enter a new journal entry.");
+        Console.WriteLine("2. Display the entire journal.");
+        Console.WriteLine("3. Save journal to a file.");
+        Console.WriteLine("4. Load a journal from a file.");
+        Console.WriteLine("5. Quit");
+        userSelection =Console.ReadLine();
+        menuChoice = int.Parse(userSelection);
+
+        while (menuChoice != 5)
+        {
+            
+        }
+
     }
 }
