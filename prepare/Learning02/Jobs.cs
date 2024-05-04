ba888cf3dfbae1435f99b2caf67f708e166298f1
Jashby1993 <144633312+Jashby1993@users.noreply.github.com>
2024-05-03 22:06:28 -0600

    completed prep program

--- prepare/Learning02/Jobs.cs
+++ prepare/Learning02/Jobs.cs
@@ -4,19 +4,12 @@ using System.Collections.Generic;
 public class Job
 {
     //defining attributes
-    private string _company;
-    private string _jobTitle;
-    private string _startYear;
-    private string _endYear;
+    public string _company;
+    public string _jobTitle;
+    public string _startYear;
+    public string _endYear;
 
-    // Constructors
-    public Job (string jobTitle, string company, string startYear, string endYear)
-    {
-        this._jobTitle = jobTitle;
-        this._company = company;
-        this._startYear = startYear;
-        this._endYear = endYear;
-    }
+
     
     //defining methods
     public void DisplayJob()
--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -6,9 +6,27 @@ class Program
     {
         Job job1 = new Job();
         job1._jobTitle = "Software Engineer";
-        job1._company = "Apple";
+        job1._company = "Microsoft";
         job1._startYear = "2014";
         job1._endYear = "2024";
-        job1.DisplayJob();
+        //job1.DisplayJob();
+        Job job2 = new Job();
+        job2._jobTitle = "Manager";
+        job2._company = "Apple";
+        job2._startYear = "2022";
+        job2._endYear = "2023";
+
+        // construct Resume
+        Resume myResume = new Resume();
+        myResume._name = "Justin Ashby";
+
+        //adding jobs
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.Display(); 
+
+
+
     }
 }
