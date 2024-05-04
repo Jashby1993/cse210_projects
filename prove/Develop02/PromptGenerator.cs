53d8a1b62f99366ed2e5567b24a3b826663ea621
Jashby1993 <144633312+Jashby1993@users.noreply.github.com>
2024-05-04 00:28:48 -0600

    finished prompt generator(?) starting on Entry

--- prove/Develop02/PromptGenerator.cs
+++ prove/Develop02/PromptGenerator.cs
@@ -1,16 +1,26 @@
 using System;
 using System.Collections.Generic;
 
+
 class PromptGenerator
 {
     // defining attributes
-     private List<string> _prompts;
-
-    // Constructor
-    public PromptGenerator()
-    {
-        // Initialize the _prompts list in the constructor
-        _prompts = new List<string>();
-    }
     
+     public List<string> _prompts = new List<string>();
+     
+     //constructor
+     public PromptGenerator()
+     {
+        //pulling the prompts from the txt file in same folder
+        string filePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "prompts.txt");
+        string[] lines = File.ReadAllLines(filePath);
+        _prompts.AddRange(lines);
+     }
+    public string GetRandomPrompt()
+    {
+        Random random = new Random();
+        int index = random.Next(0, _prompts.Count);
+        return _prompts[index];   
+    }
 }
+    
