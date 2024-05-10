00550f7a9a104b1ea0d9c3e26ffc7b0632fd763a
Justin Ashby <jashby93school@gmail.com>
2024-05-10 00:50:14 -0600

    little things

--- prove/Develop03/Word.cs
+++ prove/Develop03/Word.cs
@@ -5,17 +5,25 @@ class Word
 private string _text;
 private bool _isHidden;
 
-public Word(string _text)
+public Word(string text, bool isHidden)
 {
+    this._text = text;
+    this._isHidden = isHidden;
 
 }
 public void Hide()
 {
-
+    if (_isHidden == true)
+    {
+        _text = "_____";
+    }
 }
 public void Show()
 {
-
+    if (_isHidden == false)
+    {
+        
+    }
 }
 public bool IsHidden()
 {
