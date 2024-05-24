8ed12018d3aed9cb3f8e2590a89a5d0d9a2af0c4
Justin Ashby <jashby93school@gmail.com>
2024-05-24 00:07:10 -0600

    lot's of changes

---
+++ prove/Develop05/Level.cs
@@ -0,0 +1,15 @@
+//don't know if I'll need this, but it might come in handy
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+
+public class Level
+{
+    string _describer;
+    string _beast;
+
+    public Level(int levelNumber)
+    {
+
+    }
+}
